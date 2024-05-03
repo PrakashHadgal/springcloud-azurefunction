9e10afed0e9f73137b0016dda05f59439d5530bc
prakashhadgal <hadgalprakash92@gmail.com>
2024-05-03 11:43:48 -0400

    au

--- src/main/java/io/jaylee/springcloud/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package io.jaylee.springcloud.repository;
-
-import io.jaylee.springcloud.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface UserRepository extends JpaRepository<User, String> {
-}
